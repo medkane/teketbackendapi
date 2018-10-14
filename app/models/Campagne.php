693b04895a643296230df5f189afcce9c9d2aafb
Médoune KANE <kmedtech@gmail.com>
2018-10-14 18:15:41 +0000

    Relationship for Campagne and Cotisation

--- app/models/Campagne.php
+++ app/models/Campagne.php
@@ -2,9 +2,13 @@
 
 namespace App\models;
 
+use App\models\Cotisation;
 use Illuminate\Database\Eloquent\Model;
 
 class Campagne extends Model
 {
-    //
+    public function cotisations()
+    {
+        return $this->hasMany(Cotisation::class);
+    }
 }
--- app/models/Cotisation.php
+++ app/models/Cotisation.php
@@ -2,9 +2,13 @@
 
 namespace App\models;
 
+use App\models\Campagne;
 use Illuminate\Database\Eloquent\Model;
 
 class Cotisation extends Model
 {
-    //
+    public function campagne()
+    {
+        return $this->belongsTo(Campagne::class);
+    }
 }
